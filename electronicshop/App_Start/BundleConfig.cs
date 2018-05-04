6b6af36d9f601ddae05a6e7dd7d08520fe086cb8
onchari wycliffe <onchariwycliffe@gmail.com>
2018-05-04 18:44:20 +0300

    First

--- electronicshop/App_Start/BundleConfig.cs
+++ electronicshop/App_Start/BundleConfig.cs
@@ -25,7 +25,12 @@ namespace electronicshop
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/admin-lte/css/AdminLTE.css",
+                      "~/admin-lte/css/skins/skin-blue.css"));
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+             "~/admin-lte/js/app.js",
+             "~/admin-lte/plugins/fastclick/fastclick.js"));
         }
     }
 }
---
+++ electronicshop/Areas/CustomerFrontEnd/CustomerFrontEndAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace electronicshop.Areas.CustomerFrontEnd
+{
+    public class CustomerFrontEndAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CustomerFrontEnd";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CustomerFrontEnd_default",
+                "CustomerFrontEnd/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
